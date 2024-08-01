76ff7432271514977138c219b70a1eba21ca93e2
KhushiVikam <kikivikam@gmail.com>
2024-08-02 03:04:05 +0530

    first commit

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,48 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const resp = await fetch(url);
+        if (!resp.ok) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        const user = await resp.json();
+        setUser(user);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchUser();
+  }, []);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2> There is an error...</h2>;
+  }
+  const { avatar_url, name, company, bio } = user;
+  return (
+    <div>
+      <img
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={avatar_url}
+        alt={name}
+      />
+      <h2>{name}</h2>
+      <h4>Works at: {company}</h4>
+      <p>{bio}</p>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
