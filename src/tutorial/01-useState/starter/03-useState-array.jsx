ce786b3c01f71e576630ec5c48dffc617e3d8204
KhushiVikam <kikivikam@gmail.com>
2024-08-01 15:30:31 +0530

    first commit

---
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -0,0 +1,42 @@
+import { data } from "../../../data";
+import React from "react";
+
+const UseStateArray = () => {
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    // console.log(id);
+    // const newPeople = people.filter{(person)=>person.id!== id }
+    setPeople(people.filter((person) => person.id !== id));
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        // console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name} </h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={clearAllItems}
+      >
+        Clear Items
+      </button>
+    </div>
+  );
+};
+
+export default UseStateArray;
---
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    hobby: "reading",
+  });
+
+  const displayPerson = () => {
+    setPerson({ name: "john", age: 28, hobby: "scream at the computer" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoys: {person.hobby}</h3>
+      <button className="btn" onClick={displayPerson}>
+        Show John
+      </button>
+    </>
+  );
+};
+
+export default UseStateObject;
