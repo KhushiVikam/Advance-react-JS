b1de9f38778c69f28fecab91fc7b12f6a0f91d4f
KhushiVikam <kikivikam@gmail.com>
2024-08-05 00:27:18 +0530

    Added Global context

---
+++ src/context.jsx
@@ -0,0 +1,16 @@
+import { useContext, useState } from "react";
+import { createContext } from "react";
+
+const GlobalContext = createContext();
+
+export const useGLobalContext = () => useContext(GlobalContext);
+const AppContext = ({ children }) => {
+  const [name, setName] = useState("peter");
+  return (
+    <GlobalContext.Provider value={{ name, setName }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default AppContext;
